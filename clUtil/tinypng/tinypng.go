d19116bd35d695ea396309a2b3217cd9f7dcfde6
chenxi <chenxi@gmail.com>
2024-02-02 20:00:14 +0800

    x

--- clUtil/tinypng/tinypng.go
+++ clUtil/tinypng/tinypng.go
@@ -8,6 +8,9 @@ import (
 	"github.com/cxi7448/cxhttp/clUtil/clLog"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
 const (
@@ -40,16 +43,27 @@ type Input struct {
 	Type string `json:"type"`
 }
 
+const (
+	ROOT_DIR     = "./tmp/"
+	COMPRESS_DIR = "compress/"
+)
+
+func init() {
+	os.MkdirAll(ROOT_DIR+COMPRESS_DIR, 0777)
+}
+
 func Init(email, apikey string) {
 	EMAIL = email
 	APIKEY = apikey
 }
-func Compress(image_path, new_path string) error {
+func Compress(image_path string) (string, error) {
+	ext := image_path[strings.LastIndex(image_path, "."):]
+	new_path := fmt.Sprintf("%v%v%v_compress%v", ROOT_DIR, COMPRESS_DIR, time.Now().UnixNano(), ext)
 	// 创建Request
 	req, err := http.NewRequest(http.MethodPost, CompressingUrl, nil)
 	if err != nil {
 		clLog.Error("错误:%v", err)
-		return err
+		return "", err
 	}
 	// 将鉴权信息写入Request
 	req.SetBasicAuth(EMAIL, APIKEY)
@@ -57,36 +71,36 @@ func Compress(image_path, new_path string) error {
 	data, err := ioutil.ReadFile(image_path)
 	if err != nil {
 		clLog.Error("错误:%v", err)
-		return err
+		return "", err
 	}
 	req.Body = ioutil.NopCloser(bytes.NewReader(data))
 	// 发起请求
 	response, err := http.DefaultClient.Do(req)
 	if err != nil {
 		clLog.Error("错误:%v", err)
-		return err
+		return "", err
 	}
 
 	// 解析请求
 	data, err = ioutil.ReadAll(response.Body)
 	if err != nil {
 		clLog.Error("解析Tinypng的返回失败:%v", err)
-		return err
+		return "", err
 	}
 	result := Tinypng{}
 	err = json.Unmarshal(data, &result)
 	if err != nil {
 		fmt.Println(string(data))
 		clLog.Error("解析Tinypng的返回失败:%v", err)
-		return err
+		return "", err
 	}
 	if result.Output.Url == "" {
 		fmt.Println(string(data))
-		return fmt.Errorf("拿不到图片压缩地址")
+		return "", fmt.Errorf("拿不到图片压缩地址")
 	}
 	err = clFile.Download(result.Output.Url, new_path)
 	if err != nil {
 		clLog.Error("下载Tinypng的压缩图片地址失败:%v", err)
 	}
-	return err
+	return new_path, err
 }
