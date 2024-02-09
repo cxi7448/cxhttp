d4255a0796e3cae853e706799fb355fcb1c18079
chenxi <chenxi@gmail.com>
2024-02-09 15:14:09 +0800

    x

--- clUtil/xcompress/compress_test.go
+++ clUtil/xcompress/compress_test.go
@@ -1,13 +1,12 @@
 package xcompress
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestCompressImage(t *testing.T) {
-	//_multiple := float32(1)
-	//width := 1230
-	//width = int(float32((10/_multiple)/10) * float32(width))
-	//fmt.Println(width, float32(float32(10/_multiple)/10))
-
+	path, err := JpgToPng("a.jpg")
+	fmt.Println(path)
+	fmt.Println(err)
 }
